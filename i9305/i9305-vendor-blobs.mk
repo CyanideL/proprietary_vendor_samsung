5793f9623778864d83a9385ad8749f65f33f9f20
DerTeufel <dominik-kassel@gmx.de>
2015-01-22 21:41:56 +0100

    t0lte/i9305: use prebuilt gps hal for now
    Change-Id: Ibf14e280cf9f949024c0818d09d2f98359a1cd8c
    
    Conflicts:
    	i9305/i9305-vendor-blobs.mk

--- i9305/i9305-vendor-blobs.mk
+++ i9305/i9305-vendor-blobs.mk
@@ -61,4 +61,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/usr/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/sensors.smdk4x12.so:system/lib/hw/sensors.smdk4x12.so
+    $(LOCAL_PATH)/proprietary/system/usr/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/gps.default.so:/system/lib/hw/gps.default.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libgps.utils.so:/system/lib/libgps.utils.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libloc_adapter.so:/system/lib/libloc_adapter.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libloc_eng.so:/system/lib/libloc_eng.so
